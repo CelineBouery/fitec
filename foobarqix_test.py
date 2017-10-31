77e5b9a32c1f0fcc14c7c7743c4503f9b5670a56
CelineBouery <cbouery@gmail.com>
2017-10-31 12:08:20 +0100

    extract /7

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -8,16 +8,22 @@ def is_divisible(number, divisor):
 def foobarquix(number):
     if is_divisible(number, 3):
         return "Foo"
-    else:
+    if is_divisible(number, 5):
         return "Bar"
+    if is_divisible(number, 7):
+        return "Qix"
 
 
 class FooBarQixTestCase(unittest.TestCase):
     def test_return_Foo_when_number_is_divisible_by_3(self):
         self.assertEqual(foobarquix(3), "Foo")
 
-    def test_return_Foo_when_number_is_divisible_by_5(self):
+    def test_return_Bar_when_number_is_divisible_by_5(self):
         self.assertEqual(foobarquix(5), "Bar")
+
+
+    def test_return_Qix_when_number_is_divisible_by_7(self):
+        self.assertEqual(foobarquix(7), "Qix")
 
 
 unittest.main()
