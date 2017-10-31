ff2e1f18f00366f693fd74de597ae3297c518d19
CelineBouery <cbouery@gmail.com>
2017-10-31 14:37:16 +0100

    ajout Cas 1

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -13,23 +13,31 @@ def foobarquix(number):
         result += "Bar"
     if is_divisible(number, 7):
         result += "Qix"
-return = result
+    if result != "":
+        return result
+    else:
+        return "1"
+
 
 
 class FooBarQixTestCase(unittest.TestCase):
     def test_return_Foo_when_number_is_divisible_by_3(self):
         self.assertEqual(foobarquix(3), "Foo")
-        self.assertEqual(foobarquix(15), "FooBar")
+        self.assertEqual(foobarquix(6), "Foo")
+        self.assertEqual(foobarquix(9), "Foo")
+
 
     def test_return_Bar_when_number_is_divisible_by_5(self):
         self.assertEqual(foobarquix(5), "Bar")
 
-
     def test_return_Qix_when_number_is_divisible_by_7(self):
         self.assertEqual(foobarquix(7), "Qix")
 
-        def test_return_Qix_when_number_is_divisible_by_5_and_3(self):
-            self.assertEqual(foobarquix(15), "FooBar")
+    def test_return_FooBar_when_number_is_divisible_by_5_and_3(self):
+        self.assertEqual(foobarquix(15), "FooBar")
+
+    def test_return_1_when_number_is_1(self):
+        self.assertEqual(foobarquix(1), "1")
 
 
 unittest.main()
