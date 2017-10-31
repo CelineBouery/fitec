57d647ab75ff80304f82d19ad68527c507d3f52e
CelineBouery <cbouery@gmail.com>
2017-10-31 11:51:46 +0100

    2ème test /5

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -1,12 +1,18 @@
 import unittest
 
-def foobarquix (number):
-    return "foo"
+def foobarquix(number):
+    if number % 3 == 0:
+        return "Foo"
+    else:
+        return "Bar"
+
 
 class FooBarQixTestCase(unittest.TestCase):
     def test_return_Foo_when_number_is_divisible_by_3(self):
         self.assertEqual(foobarquix(3), "Foo")
 
+    def test_return_Foo_when_number_is_divisible_by_5(self):
+        self.assertEqual(foobarquix(5), "Bar")
 
 
 unittest.main()
